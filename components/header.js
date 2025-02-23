fe4f9598bbba50c65bedf2e35f2f29cda8d572af
Khang Quách <khangB2203505@student.ctu.edu.vn>
2025-02-23 16:57:16 +0700

    .

---
+++ components/header.js
@@ -0,0 +1,9 @@
+export default function Header(){
+  return (
+    // <div Stype="w-screen h-20 bg-blue-500 z-10" >
+    // </div>
+    <div style={{ width: '100vw', height: '70px', backgroundColor: 'black' }}>
+      
+    </div>
+  )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 "use client"
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import '@xyflow/react/dist/style.css';
 import {
   ReactFlow,
   MiniMap,
@@ -8,38 +9,92 @@ import {
   useNodesState,
   useEdgesState,
   addEdge,
+  Panel,
+  ControlButton,
 } from '@xyflow/react';
- 
-import '@xyflow/react/dist/style.css';
- 
+import Header from '../../components/header';
+
 const initialNodes = [
-  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
-  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+  {
+    id: '1',
+    data: { label: '1' },
+    position: { x: 0, y: 0 },
+    type: 'input',
+  },
+  {
+    id: '2',
+    data: { label: '2' },
+    position: { x: 100, y: 100 },
+  },
+  {
+    id: '3',
+    data: { label: '3' },
+    position: { x: 300, y: 300 },
+  },
 ];
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
- 
+const initialEdges = [
+  {
+    id: 'e1-2',
+    source: '1',
+    target: '2'
+  },
+  {
+    id: 'e2-3',
+    source: '2',
+    target: '3'
+  }
+];
 export default function App() {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
- 
+  const [selectedNode, setSelectedNode] = useState(null);
+  const [selectedEdge, setSelectedEdge] = useState(null);
   const onConnect = useCallback(
     (params) => setEdges((eds) => addEdge(params, eds)),
     [setEdges],
   );
- 
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.key === 'Delete' && selectedNode) {
+        console.log('Deleted node')
+        setNodes(initialNodes.filter(node => node.id !== selectedNode.id))
+        setEdges(initialEdges.filter(edge => edge.source !== selectedNode.id && edge.target !== selectedNode.id));
+        setSelectedNode(null)
+      }
+    };
+
+    document.addEventListener('keydown', handleKeyDown);
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [selectedNode]);
+
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
-      <ReactFlow
-        nodes={nodes}
-        edges={edges}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        onConnect={onConnect}
-      >
-        <Controls />
-        <MiniMap />
-        <Background color="#333" variant="dots" gap={12} size={1} />
-      </ReactFlow>
-    </div>
+    <>
+      <Header />
+      <div style={{ width: '100vw', height: '80vh' }}>
+        <ReactFlow
+          nodes={nodes}
+          edges={edges}
+          onNodesChange={onNodesChange}
+          onEdgesChange={onEdgesChange}
+          onConnect={onConnect}
+          onNodeClick={(event, node) => {
+            setSelectedNode(node)
+            console.log(node)
+          }}
+          onEdgeClick={(event, edge) => {
+            setSelectedEdge(edge)
+            console.log(edge)
+          }}
+        >
+          <MiniMap />
+          <Background color="#333" variant="dots" gap={12} size={1} />
+          <Controls>
+          </Controls>
+        </ReactFlow>
+      </div>
+    </>
   );
 }
